cd1524e438a8bf021096d65f5a62ebde6b9dd58f
zhangqiang <zhangqiang@wifi.com>
2017-03-21 15:01:51 +0800

    优化

--- djinn/options.py
+++ djinn/options.py
@@ -30,7 +30,7 @@ def parse_config_file(filename):
     from a configuration file.
     """
     config = {}
-    execfile(filename, {}, config)
+    execfile(filename, locals=config)
     for name in config:
         if name in options._options:
             options._options[name].set(config[name])
